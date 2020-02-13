df70a1d93eb02721ad69f5cd7a2ce12bcd260fba
geimaj <16000925@vcconnect.co.za>
2020-02-14 01:37:17 +0200

    deploy

---
+++ config/development.js
@@ -0,0 +1,5 @@
+module.exports.host = "localhost";
+module.exports.url = "http://localhost:3030";
+module.exports.env = "development";
+module.exports.dbURL = "mongodb://localhost:27017/property24";
+module.exports.sessionSecret = "qwertyuiop123456789";
---
+++ config/production.js
@@ -0,0 +1,5 @@
+module.exports.host = "localhost";
+module.exports.url = process.env.URL;
+module.exports.env = "production";
+module.exports.dbURL = process.env.DBURL;
+module.exports.sessionSecret = "qwertyuiop123456789";
