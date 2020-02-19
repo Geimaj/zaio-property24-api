a0206e1890c00defd55e2e4cb67bb923e28e7f9e
geimaj <16000925@vcconnect.co.za>
2020-02-20 00:20:56 +0200

    remove nested error object in signup response

--- routes/signup.js
+++ routes/signup.js
@@ -2,7 +2,6 @@ const signupRouter = require("express").Router();
 const { User } = require("../models/User");
 
 signupRouter.route("/").post(async (req, res, next) => {
-<<<<<<< HEAD
   const { username, password, email, fullname } = req.body;
   try {
     //validate
@@ -18,44 +17,27 @@ signupRouter.route("/").post(async (req, res, next) => {
     if ((await existingUsers).length > 0) {
       throw new Error("username is taken");
     }
-=======
-	const { username, password, email, fullname } = req.body;
-	try {
-		//validate
-		if (!username) {
-			throw new Error("{error: 'usernamename is required'}");
-		}
-		if (!password) {
-			throw new Error("{error: 'password is required'}");
-		}
 
-		const existingUsers = await User.find({ username: username });
-		//ensure username is available
-		if ((await existingUsers).length > 0) {
-			throw new Error("{error: 'username is taken'}");
-		}
->>>>>>> de30977dbf371be41c9158ab8ad94148d7f5781b
-
-		new User({
-			username: username,
-			email: email,
-			fullname: fullname
-		})
-			.save()
-			.then(user => user.setPassword(password))
-			.then(user =>
-				res.send({
-					_id: user._id,
-					username: user.username,
-					email: user.email,
-					fullname: user.fullname
-				})
-			)
-			.catch(next);
-	} catch (err) {
-		console.log("nexting...");
-		next(err);
-	}
+    new User({
+      username: username,
+      email: email,
+      fullname: fullname
+    })
+      .save()
+      .then(user => user.setPassword(password))
+      .then(user =>
+        res.send({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          fullname: user.fullname
+        })
+      )
+      .catch(next);
+  } catch (err) {
+    console.log("nexting...");
+    next(err);
+  }
 });
 
 module.exports = signupRouter;
