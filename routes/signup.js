de30977dbf371be41c9158ab8ad94148d7f5781b
geimaj <16000925@vcconnect.co.za>
2020-02-19 20:28:48 +0200

    fix import

--- routes/signup.js
+++ routes/signup.js
@@ -1,43 +1,43 @@
 const signupRouter = require("express").Router();
-const User = require("../models/User");
+const { User } = require("../models/User");
 
 signupRouter.route("/").post(async (req, res, next) => {
-  const { username, password, email, fullname } = req.body;
-  try {
-    //validate
-    if (!username) {
-      throw new Error("{error: 'usernamename is required'}");
-    }
-    if (!password) {
-      throw new Error("{error: 'password is required'}");
-    }
+	const { username, password, email, fullname } = req.body;
+	try {
+		//validate
+		if (!username) {
+			throw new Error("{error: 'usernamename is required'}");
+		}
+		if (!password) {
+			throw new Error("{error: 'password is required'}");
+		}
 
-    const existingUsers = await User.find({ username: username });
-    //ensure username is available
-    if ((await existingUsers).length > 0) {
-      throw new Error("{error: 'username is taken'}");
-    }
+		const existingUsers = await User.find({ username: username });
+		//ensure username is available
+		if ((await existingUsers).length > 0) {
+			throw new Error("{error: 'username is taken'}");
+		}
 
-    new User({
-      username: username,
-      email: email,
-      fullname: fullname
-    })
-      .save()
-      .then(user => user.setPassword(password))
-      .then(user =>
-        res.send({
-          _id: user._id,
-          username: user.username,
-          email: user.email,
-          fullname: user.fullname
-        })
-      )
-      .catch(next);
-  } catch (err) {
-    console.log("nexting...");
-    next(err);
-  }
+		new User({
+			username: username,
+			email: email,
+			fullname: fullname
+		})
+			.save()
+			.then(user => user.setPassword(password))
+			.then(user =>
+				res.send({
+					_id: user._id,
+					username: user.username,
+					email: user.email,
+					fullname: user.fullname
+				})
+			)
+			.catch(next);
+	} catch (err) {
+		console.log("nexting...");
+		next(err);
+	}
 });
 
 module.exports = signupRouter;
