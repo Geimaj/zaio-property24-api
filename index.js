3353ac00fc5ac3eb00a2948acf05529f1d28f3d3
geimaj <16000925@vcconnect.co.za>
2020-02-20 13:48:02 +0200

    return user details on login

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const propertyRouter = require("./routes/property");
 const app = express();
 
 const config = require("./config/config"),
-	port = process.env.PORT || 3030,
-	{ env, dbURL } = config;
+  port = process.env.PORT || 3030,
+  { env, dbURL } = config;
 
 app.set("port", port);
 app.set("env", env);
@@ -25,27 +25,28 @@ app.set("env", env);
 const connectionString = dbURL;
 console.log(`connectionString ${connectionString}`);
 mongoose
-	.connect(connectionString, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useNewUrlParser: true
-	})
-	.catch(err => {
-		console.log("error");
-		console.log(err);
-	});
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .catch(err => {
+    console.log("error connecting to database");
+    console.log(err);
+    return;
+  });
 
 //cors
 //set Access-Control-Allow-Credentials header
 app.use((req, res, next) => {
-	console.log(`${req.method} ${req.path}`);
-	res.set("Access-Control-Allow-Credentials", "true");
-	next();
+  console.log(`${req.method} ${req.path}`);
+  res.set("Access-Control-Allow-Credentials", "true");
+  next();
 });
 app.use(
-	cors({
-		origin: true
-	})
+  cors({
+    origin: true
+  })
 );
 app.options("*", cors());
 
@@ -62,13 +63,13 @@ app.use("/login", loginRouter);
 
 //make sure all other requests are authenticated
 app.use((req, res, next) => {
-	if (!req.user) {
-		res.status(401);
-		res.send({ error: "unauthorized request" });
-		return;
-	} else {
-		next();
-	}
+  if (!req.user) {
+    res.status(401);
+    res.send({ error: "unauthorized request" });
+    return;
+  } else {
+    next();
+  }
 });
 // every route below this line will fail without auth
 
@@ -77,18 +78,18 @@ app.use("/user", userRouter);
 app.use("/property", propertyRouter);
 
 app.get("/", (req, res) => {
-	if (req.user) {
-		res.send({ message: "welcome" });
-	} else {
-		console.log("no auth");
-	}
+  if (req.user) {
+    res.send({ message: "welcome" });
+  } else {
+    console.log("no auth");
+  }
 });
 
 //error handling
 app.use((err, req, res, next) => {
-	console.log(err);
-	res.status(422).send({ error: err.message });
-	next(err.message);
+  console.log(err);
+  res.status(422).send({ error: err.message });
+  next(err.message);
 });
 
 app.listen(port);
--- routes/login.js
+++ routes/login.js
@@ -2,26 +2,32 @@ const loginRouter = require("express").Router();
 const passport = require("../config/passport");
 
 loginRouter
-	.route("/")
-	.post(
-		passport.authenticate("local", {
-			failureRedirect: "/login?fail=true",
-			successRedirect: "/"
-		})
-	)
-	.options(
-		passport.authenticate("local", {
-			failureRedirect: "/login?fail=true",
-			successRedirect: "/"
-		})
-	)
-	.get((req, res) => {
-		if (req.query.fail) {
-			res.status(401);
-			res.send({ error: "Invalid creds" });
-		} else {
-			res.send({ message: "login page" });
-		}
-	});
+  .route("/")
+  .post(
+    passport.authenticate("local", {
+      failureRedirect: "/login?fail=true"
+    }),
+    handleSuccesss
+  )
+  .options(
+    passport.authenticate("local", {
+      failureRedirect: "/login?fail=true"
+    }),
+    handleSuccesss
+  )
+  .get((req, res) => {
+    if (req.query.fail) {
+      res.status(401);
+      res.send({ error: "Invalid creds" });
+    } else {
+      res.send({ message: "login page" });
+    }
+  });
+
+function handleSuccesss(req, res) {
+  if (req.user) {
+    res.redirect(`/user/${req.user.id}`);
+  }
+}
 
 module.exports = loginRouter;
