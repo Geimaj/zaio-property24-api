55e5f985ef93d874e0a7b40a7a6552c24c4aa39d
geimaj <16000925@vcconnect.co.za>
2020-02-20 10:22:36 +0200

    add city field

--- models/Property.js
+++ models/Property.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const propertySchema = new Schema({
-	street: String,
-	number: String,
-	beds: Number,
-	baths: Number,
-	price: Number,
-	postedBy: Schema.Types.ObjectId,
-	images: Array,
-	postCode: String,
-	name: String
+  street: String,
+  number: String,
+  beds: Number,
+  baths: Number,
+  price: Number,
+  postedBy: Schema.Types.ObjectId,
+  images: Array,
+  postCode: String,
+  name: String,
+  city: String
 });
 
 const Property = mongoose.model("Property", propertySchema);
